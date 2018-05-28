fe423032d6feac2b62be9b5a805f4c4ae544b724
crosmatthieu <36661700+crosmatthieu@users.noreply.github.com>
2018-05-28 17:36:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 let bot = new Discord.Client()
-
+bot.on("ready", () => console.log("Connected"))
 bot.on("message", async message =>{
   if(message.content == "//ping") message.reply(bot.ping)
 })
