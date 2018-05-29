d18ecb8e02b97c510ef152d548e1fd3352efe138
crosmatthieu <36661700+crosmatthieu@users.noreply.github.com>
2018-05-29 19:59:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ let bot = new Discord.Client()
 bot.on("ready", () => console.log("Connected"))
 bot.on("message", async message =>{
   if(message.content == "//ping") message.channel.send(bot.ping)
-  if(message.content == "//test") message.channe.send(fs.readFileSync("./test.txt", "utf8"))
+  if(message.content == "//test") message.channel.send(fs.readFileSync("./test.txt", "utf8"))
 })
 bot.login(process.env.token)
